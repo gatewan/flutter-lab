0de76fdf0168dcf3bd66faf708e03c83f5e48e66
danielfb79 <90594889+danielfb79@users.noreply.github.com>
2023-06-13 13:54:33 +0700

    Import Assets to Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lab/pages/home_page.dart';
+import 'package:flutter_lab/pages/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const SplashPage(),
     );
   }
 }
