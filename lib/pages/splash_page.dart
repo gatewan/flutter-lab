7f1ccbc1042ec2c139de93fb64fb855d1912ed36
danielfb79 <90594889+danielfb79@users.noreply.github.com>
2023-06-13 15:24:21 +0700

    Build UI Splash Page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -6,10 +6,68 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset("assets/images/welcome_splash.png"),
-        ],
+      backgroundColor: Colors.white,
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              "Welcome to MyCodelab",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 24,
+              ),
+              textWidthBasis: TextWidthBasis.parent,
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 20,
+              ),
+              child: Text(
+                "The world needs a superhero like you!",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Image.asset(
+              "assets/images/welcome_splash.png",
+              width: 320,
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.deepPurpleAccent,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 52,
+                  vertical: 8,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                textStyle: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              onPressed: () {},
+              child: Text("OPEN"),
+            ),
+          ],
+        ),
       ),
     );
   }
