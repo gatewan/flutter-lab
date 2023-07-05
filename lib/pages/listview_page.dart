7ee2b847a08f0c934482678486bace5fb043f675
danielfb79 <90594889+danielfb79@users.noreply.github.com>
2023-07-05 13:17:18 +0700

    Changes: - Upgraded HomePage - Explore ListView Widget (horizontal & vertical)

--- lib/pages/listview_page.dart
+++ lib/pages/listview_page.dart
@@ -10,237 +10,280 @@ class ListViewPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
+            height: 200,
             width: double.infinity,
-            height: 100,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("1"),
+            child: ListTile(
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 24, horizontal: 18),
+              title: Text(
+                "ListView",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 40),
+              ),
+              subtitle: Text(
+                "A scrollable list of widgets arranged linearly. ListView is the most commonly used scrolling widget. It displays its children one after another in the scroll direction. In the cross axis, the children are required to fill the ListView.",
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 12),
+            child: SizedBox(
+              width: double.infinity,
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("1"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("2"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("2"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("3"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("3"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("4"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("4"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("5"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("5"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("6"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("6"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("7"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("7"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("8"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("8"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("9"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                          width: 76,
+                          height: 76,
+                          child: Center(
+                            child: Text("9"),
+                          )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Card(
-                    color: Colors.grey,
-                    child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("10"),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Card(
+                      color: Colors.white,
+                      child: SizedBox(
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("10"),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           SizedBox(
             width: 100,
-            height: MediaQuery.of(context).size.height - 100,
+            height: MediaQuery.of(context).size.height - 312,
             child: ListView(
               children: const [
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("1"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("1"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("2"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("2"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("3"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("3"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("4"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("4"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("5"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("5"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("6"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("6"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("7"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("7"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("8"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("8"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("9"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("9"),
+                        )),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(8),
                   child: Card(
-                    color: Colors.grey,
+                    color: Colors.white,
                     child: SizedBox(
-                      width: 76,
-                      height: 76,
-                      child: Text("10"),
-                    ),
+                        width: 76,
+                        height: 76,
+                        child: Center(
+                          child: Text("10"),
+                        )),
                   ),
                 ),
               ],
